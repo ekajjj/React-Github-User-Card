ddecb42a4f196be4dcae2cb1e28daa89c7ab9386
Jake Johnson <jakejjohnson@gmail.com>
2020-02-11 16:03:53 -0600

    setup

---
+++ github-usercard/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  render()
+  {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
